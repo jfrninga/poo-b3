ce2f4f30290ae448ad196df812dfe6c977fdfbfd
jfr <jfrsimbare@gmail.com>
2022-10-19 10:32:29 +0200

    first page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<div  class="base">
+<h1>My first blog post</h1>
+<p>
+    Anim irure mollit qui id veniam et ut dolor Lorem mollit ut.<br>
+    Minim velit est anim esse nulla proident non consectetur officia.<br>
+    In Lorem est amet labore commodo laboris veniam reprehenderit ullamco labore elit.<br>
+    Consectetur reprehenderit culpa minim qui cupidatat irure ut fugiat velit reprehenderit incididunt sit ad anim.
+</p>
+</div>
+
+<style>
+    .base{
+        text-align: center;
+    }
+</style>
+
+
+</body>
+</html>
